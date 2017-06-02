8b16981949f432070dee7e5e50ceec6a7a5bdc9e
ynakahashi <ynakahashi1003@gmail.com>
2017-06-02 23:13:27 +0900

    no message

--- AdStock_Estimation.R
+++ AdStock_Estimation.R
@@ -13,91 +13,103 @@
 work_dir <- "/Users/nakahashi/Desktop/GitTest/MarketingMixModeling"
 setwd(work_dir)
 
+## libraries
 library(dplyr)
+
 
 ################################################################################
 ## Palda model
 ################################################################################
 simulate_y <- function(pars) {
-   n      <- pars[1] # num of observation
-   mu     <- pars[2] # intercept
-   beta   <- pars[3] # regression coefficient to be esitmated
-   lambda <- pars[4] # decay rate of Ad-Stock effect
-   var_e  <- pars[5] # residual variance
+   n         <- pars[1] # num of observation
+   mu        <- pars[2] # intercept
+   beta_01   <- pars[3] # regression coefficient of X1 to be esitmated
+   beta_02   <- pars[4] # regression coefficient of X2 to be esitmated
+   lambda_01 <- pars[5] # decay rate of Ad-Stock effect of X1
+   lambda_02 <- pars[6] # decay rate of Ad-Stock effect of X2
+   var_e     <- pars[7] # residual variance
    
-   X_raw <- rnorm(n, 100, 2) * ifelse(runif(n) > 0.7, 0, 1) # X
-   X_fil <- stats::filter(X_raw, lambda, "recursive") # filter-ed X
-   error <- rnorm(n, 0, sqrt(var_e)) # white noise
-   y     <- mu + beta * X_fil + error # create y
+   X_01_raw <- rnorm(n, 100, 2) * ifelse(runif(n) > 0.7, 0, 1)
+   X_01_fil <- stats::filter(X_01_raw, lambda_01, "recursive")
 
+   X_02_raw <- rnorm(n, 100, 2) * ifelse(runif(n) > 0.2, 0, 1)
+   X_02_fil <- stats::filter(X_02_raw, lambda_02, "recursive")
+   
+   error <- rnorm(n, 0, sqrt(var_e))
+
+   y     <- mu + beta_01 * X_01_fil + beta_02 * X_02_fil + error
    dat <- data.frame(
       "Y" = y,
-      "X" = X_raw,
+      "X_01" = X_01_raw,
+      "X_02" = X_02_raw,
       "Y_lag" = dplyr::lag(y, 1))
    return(na.omit(dat))
 }
 
 ## check
-init_par <- array(c(100, 5, 0.5, 0.9, 2))
+init_par <- array(c(100, 5, 0.5, 0.3, 0.9, 0.2, 2))
 simulate_y(init_par)
 
 
 ## simulation
 set.seed(123)
-init_par <- array(c(100, 5, 0.5, 0.9, 2))
-k        <- 500
-results  <- matrix(0, k, 3)
+init_par      <- array(c(100, 5, 0.5, 0.3, 0.9, 0.2, 5))
+k             <- 500
+results_palda <- matrix(0, k, 4)
 for (i in 1:k) {
    dat <- simulate_y(init_par)
-   res <- lm(Y ~ ., data = dat)
-   results[i, 1:3] <- coef(res)
+   res <- lm(Y ~ X_01 + X_02 + Y_lag, data = dat)
+   results_palda[i, ] <- coef(res)
 }
 
-summary(results[, 2])
-MASS::truehist(results[, 2])
-summary(results[, 3])
-MASS::truehist(results[, 3])
-
+summary(results_palda)
+MASS::truehist(results_palda[, 1])
+MASS::truehist(results_palda[, 2])
+MASS::truehist(results_palda[, 3])
+MASS::truehist(results_palda[, 4])
 
 
 ################################################################################
 ## Optim
 ################################################################################
-simulate_y_02 <- function(pars) {
-   X1 <- rnorm(100, 100, 2) * rep(c(0, rep(1, 7), 0, 1), 10)
-   X2 <- filter(X1, pars[1], "recursive")
-   Z1 <- rnorm(100, 5, 2) * rep(c(rep(0, 5), rep(1, 5)), 10)
-   Z2 <- filter(Z1, pars[2], "recursive")
-   dat <- data.frame(
-      "Y"  = 50 + pars[3] * X2 + pars[4] * Z2 + rnorm(100, 0, pars[5]),
-      "X1" = X1,
-      "Z1" = Z1)
-   return(dat)
-}
-
-return_AIC_02 <- function(param) {
+return_AIC <- function(param) {
    a <- param[1]
    b <- param[2]
-   dat$TmpX <- filter(dat$X1, a, "recursive")
-   dat$TmpZ <- filter(dat$Z1, b, "recursive")
-   AIC(lm(Y ~ TmpX + TmpZ, dat))
+   dat$X_01_fil <- stats::filter(dat$X_01, a, "recursive")
+   dat$X_02_fil <- stats::filter(dat$X_02, b, "recursive")
+   AIC(lm(Y ~ X_01_fil + X_02_fil, dat))
 }
 
-init_par <- array(c(0.5, 0.5))
-optim(par = optim(par = init_par, fn = return_AIC)$par, 
+## check
+init_par  <- array(c(100, 5, 0.5, 0.3, 0.9, 0.2, 2))
+dat       <- simulate_y(init_par)
+decay_par <- array(c(0.5, 0.5))
+optim(par = optim(par = decay_par, fn = return_AIC)$par, 
       fn = return_AIC)$par
 
-pars      <- c(0.8, 0.7, 0.5, 4, 5.0)
-n         <- 100
-res_02_02 <- matrix(0, n, 5)
-for (i in 1:n) {
-   dat <- simulate_y(pars)
-   res <- optim(par = optim(par = init_par, fn = return_AIC)$par, 
+
+set.seed(456)
+init_par      <- array(c(100, 5, 0.5, 0.3, 0.9, 0.2, 5))
+decay_par     <- array(c(0.5, 0.5))
+k             <- 500
+results_optim <- matrix(0, k, 5)
+for (i in 1:k) {
+   dat <- simulate_y(init_par)
+   res <- optim(par = optim(par = decay_par, fn = return_AIC)$par, 
                 fn = return_AIC)$par
-   X2  <- filter(dat$X1, res[1], "recursive")
-   Z2  <- filter(dat$Z1, res[2], "recursive")
-   res_02_02[i, ] <- c(res, coef(lm(Y ~ X2 + Z2, data = dat)))
+   
+   dat$X_01_fil <- stats::filter(dat$X_01, res[1], "recursive")
+   dat$X_02_fil <- stats::filter(dat$X_02, res[2], "recursive")
+
+   lm_out             <- lm(Y ~ X_01_fil + X_02_fil, data = dat)
+   results_optim[i, ] <- c(res, coef(lm_out))
+   if (i %% 50 == 0) cat("Round: ", i, "\n")
 }
-summary(res_02_02)
+
+summary(results_optim)
+MASS::truehist(results_optim[, 1])
+MASS::truehist(results_optim[, 2])
+MASS::truehist(results_optim[, 3])
+MASS::truehist(results_optim[, 4])
 
 
